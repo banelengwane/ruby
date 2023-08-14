ae7a117d0f178154735ccc3413ea5fb7eadfef30
Banele Ngwane <banele.ngwane@inndx.net>
2023-08-14 20:52:36 +0200

    fully functioning RPS game

--- oop_rockpaperscisors.rb
+++ oop_rockpaperscisors.rb
@@ -4,9 +4,10 @@
 class Player 
     MOVES = [:rock, :paper, :scissors]
   
-    attr_reader :score, :move
+    attr_reader :score, :move, :name
     
-    def initialize
+    def initialize(name)
+      @name = name
       @score = 0
       @move = nil
     end
@@ -16,12 +17,26 @@ class Player
         puts "Pick a move."
         print "> "
         @move =  gets.chomp.strip.downcase.to_sym
-        if MOVES.include?(@move)
+        if @move == :quit
+          return false
+        elsif MOVES.include?(@move)
           return @move
         else
           puts "invalid move."
         end
       end
+    end
+  
+    def win
+      @score += 1
+    end 
+    
+  end
+  
+  # lets make a computer player
+  class ComputerPlayer < Player
+    def get_move
+      @move = [:rock, :paper, :scissors].sample
     end
   end
   
@@ -34,19 +49,56 @@ class Player
     ]
   
     def initialize
-      @player1 = Player.new
-      @player2 = Player.new
+      @players = []
+      @players << @player1 = Player.new("You")
+      @players << @player2 = ComputerPlayer.new("Computer")
+      play
     end
-    
+  
     #moves as [:rock, :scissors]
-    def pick_winner(moves)
-      if moves[0] == moves[1]
+    def pick_winner
+      if @player1.move == @player2.move
         puts "It's a tie!"
-      elsif WIN_SCENARIOS.include?(moves)
-        puts "Player 1 wins"
+      elsif WIN_SCENARIOS.include?([@player1.move, @player2.move])
+        puts "You win"
+        @player1.win
       else 
-        puts "Player 2 wins"
+        puts "Computer wins"
+        @player2.win
       end
     end 
+  
+    def play_round 
+      return false unless @players.all? do |player|
+        player.get_move
+      end
+      print_moves
+      pick_winner
+      print_scores
+      puts
+      puts "Let's play again."
+      true
+    end
+  
+    def play
+      loop do 
+        break unless play_round
+      end
+      puts "Goodbye, thanks for playing."
+    end
+  
+    def print_moves
+      @players.each do |player|
+        puts "#{player.name} played #{player.move}."
+      end
+    end
+  
+    def print_scores
+      puts "Scores: "
+      @players.each do |player|
+         puts "#{player.name}: #{player.score} Point(s)"
+      end
+    end
   end
   
+  Game.new
