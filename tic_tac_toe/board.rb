372fe641f9fe63f2439340f73fcf66ebd503b661
Banele Ngwane <banele.ngwane@inndx.net>
2023-08-18 11:25:01 +0200

    Check columns for winner

--- tic_tac_toe/board.rb
+++ tic_tac_toe/board.rb
@@ -25,26 +25,35 @@ class Board
         end
     end
 
-    def column_win?
+    def column_win?(marker)
+        (0...WIDTH).any? do |column|
+            @grid.all? do |row|
+                row[column] == marker
+            end
+        end
     end
 
     def diagonal_win?
     end
 end
 
+#test area
 def print_and_check
     @b.print_grid
-    p @b.row_win?(:O)
-    p @b.row_win?(:X)
+    puts "Row with all 0s: #{@b.row_win?(:O)}"
+    puts "Row with all Xs: #{@b.row_win?(:X)}"
+    puts "Column with all 0s: #{@b.column_win?(:O)}"
+    puts "Column with all 0s: #{@b.column_win?(:O)}"
+    
 end
 
 @b = Board.new
 print_and_check
-@b.grid[0][0] = :O
-print_and_check
-@b.grid[0][1] = :O
-print_and_check
 @b.grid[0][2] = :O
+print_and_check
+@b.grid[1][1] = :O
+print_and_check
+@b.grid[2][0] = :O
 print_and_check
 
 
