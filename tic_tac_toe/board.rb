b57eda4b4e4e937bb2dde1db46c4281540cca93f
banelengwane <banelengwane@gmail.com>
2023-08-21 20:54:59 +0200

    Numbering and lettering the grid, for user-friendliness

--- tic_tac_toe/board.rb
+++ tic_tac_toe/board.rb
@@ -6,39 +6,8 @@ class Board
         @grid = Array.new(HEIGHT) {Array.new(WIDTH,:" ")}
     end
 
-    def display
-        @grid.reduce("\n") { |output, row| output << format_row(row) } << "\n"
-    end
-
-    def row_win?(marker)
-        @grid.any? do |row|
-            row.all? do |cell|
-                cell == marker
-            end
-        end
-    end
-
-    def column_win?(marker)
-        (0...WIDTH).any? do |column|
-            @grid.all? do |row|
-                row[column] == marker
-            end
-        end
-    end
-
     def winner?(marker)
         row_win?(marker) || column_win?(marker) || diagonal_win?(marker)
-    end
-
-    def diagonal_win?(marker)
-        [
-            lambda {|i| i},
-            lambda {|i| -(i+1)}
-        ].any? do |proc|
-            (0...HEIGHT).all? do |i|
-                @grid[i][proc.call(i)] == marker
-            end
-        end
     end
 
     # getter
@@ -57,9 +26,58 @@ class Board
         end
     end
 
+    def display
+        output = "\n"
+        output << generate_header
+        #TODO : GENERATE grid with letter for each row
+        output << generate_rows
+        output << "\n"
+    end
+
     private
-    def format_row(row )
-        row.reduce("") { |row_string, cell| row_string << "[#{cell}]"} << "\n"
+
+    def format_row(row, letter)
+        row.reduce("  #{letter} ") { |string, cell| string << "[#{cell}]"} << "\n"
+    end
+
+    def generate_header
+        (1..WIDTH).reduce("   ") { |header, n| header << "  #{n}"} << "\n"
+    end
+
+    def generate_rows
+        letter = "@"
+        @grid.reduce("") do |output, row| 
+            letter = letter.next
+            output << format_row(row, letter) 
+        end
+        
+    end 
+
+    def row_win?(marker)
+        @grid.any? do |row|
+            row.all? do |cell|
+                cell == marker
+            end
+        end
+    end
+
+    def column_win?(marker)
+        (0...WIDTH).any? do |column|
+            @grid.all? do |row|
+                row[column] == marker
+            end
+        end
+    end
+
+    def diagonal_win?(marker)
+        [
+            lambda {|i| i},
+            lambda {|i| -(i+1)}
+        ].any? do |proc|
+            (0...HEIGHT).all? do |i|
+                @grid[i][proc.call(i)] == marker
+            end
+        end
     end
 end
 
