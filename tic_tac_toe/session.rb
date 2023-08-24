0a2b3ca59b75edd6b4773d9a468bf239fc057e9a
banelengwane <banelengwane@gmail.com>
2023-08-24 19:43:14 +0200

    Play again functionality added

--- tic_tac_toe/session.rb
+++ tic_tac_toe/session.rb
@@ -11,7 +11,24 @@ class Session
             Player.new("Maya", :O)
         ]
 
-        play_a_game
+        play_loop
+    end
+
+    private
+
+    def play_loop
+        loop do 
+            puts "Starting a new game."
+            play_a_game   
+            break unless play_again?   
+        end
+        puts "Goodbye"
+    end
+
+    def play_again?
+        print "Play again? (Y/N)\n>"
+        answer = gets.strip[0].upcase
+        answer = "Y"    
     end
 
     def play_a_game
